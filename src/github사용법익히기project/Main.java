2fb92979a7356983ef04f0f588aa6b987582dd56
Yujin <jyj9747@naver.com>
2022-01-03 22:07:50 +0900

    first commit

--- src/github사용법익히기project/Main.java
+++ src/github사용법익히기project/Main.java
@@ -5,8 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("I am yujin and I am a CEO") ;
 		
-		// freshman
-		System.out.println("This section made by a freshman") ;
 	}
 
 }
